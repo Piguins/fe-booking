7204462c935126b86355575632f487a1c3df5765
kiethongngu <21522263@gm.uit.edu.vn>
2023-12-07 11:06:05 +0700

    Hotel page

---
+++ client/src/pages/hotel/Hotel.jsx
@@ -0,0 +1,104 @@
+import Header from '../../components/header/Header';
+import Navbar from '../../components/navbar/Navbar';
+import Footer from '../../components/footer/Footer';
+import MailList from '../../components/mailList/MailList';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { DateRange } from 'react-date-range';
+import { format } from 'date-fns';
+import './hotel.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faCalendar, faHeart, faLocationDot, faMagnifyingGlass, faQuestion, faShareNodes } from '@fortawesome/free-solid-svg-icons';
+
+const Hotel = () => {
+  const [openStartDate, setOpenStartDate] = useState(false);
+  const [openOption, setOpenOption] = useState(false);
+  const [openEndDate, setOpenEndDate] = useState(false);
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
+
+
+
+
+  const handleSub = (name) => {
+    setOptions(prev => { 
+        return {...prev, [name]: options[name] - 1}
+    });
+  };
+  const handleAdd = (name) => {
+      setOptions(prev => { 
+          return {...prev, [name]: options[name] + 1}
+      });
+  };
+
+  // const photos = [
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max500/349023670.jpg?k=8bca080ed4abb6c663726415174889c446943173e2ac6fa00b51458f28ba0fd1&o=&hp=1"
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max500/349023670.jpg?k=8bca080ed4abb6c663726415174889c446943173e2ac6fa00b51458f28ba0fd1&o=&hp=1"
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max500/349023670.jpg?k=8bca080ed4abb6c663726415174889c446943173e2ac6fa00b51458f28ba0fd1&o=&hp=1"
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max500/349023670.jpg?k=8bca080ed4abb6c663726415174889c446943173e2ac6fa00b51458f28ba0fd1&o=&hp=1"
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max500/349023670.jpg?k=8bca080ed4abb6c663726415174889c446943173e2ac6fa00b51458f28ba0fd1&o=&hp=1"
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max500/349023670.jpg?k=8bca080ed4abb6c663726415174889c446943173e2ac6fa00b51458f28ba0fd1&o=&hp=1"
+  //   }
+  // ];
+  return (
+    <div className='hotel'>
+      <Navbar />
+      <Header type='list' />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <div className="hotelSearch">
+            <h1 className="htTitle">Search</h1>
+            <div className="htItem">
+              <label htmlFor="">Destination/property name:</label>
+              <div className="htInput">
+                <FontAwesomeIcon icon={faMagnifyingGlass} className="icon"/>
+                <input 
+                  type="text" 
+                  placeholder='Where are you going?'   
+                  value={destination} 
+                  onChange={e => setDestination(e.target.value)}
+                  />
+              </div>
+            </div>
+            <label htmlFor="">Check-in date</label> 
+            <div className="htItem">
+              <div className="htDate" onClick={() => setOpenStartDate(!openStartDate)}>
+                <FontAwesomeIcon icon={faCalendar} className='icon'/>
+                <span>
+                  {format(date[0].startDate,"MM/dd/yyyy")}
+                </span>
+                <FontAwesomeIcon icon={faAngleDown} className='htAngle'/>
+              </div>
+              
+            </div>
+            <label htmlFor="">Check-out date</label> 
+            <div className="htItem">
+              <div className="htDate" onClick={() => setOpenEndDate(!openEndDate)}>
+                <FontAwesomeIcon icon={faCalendar} className='icon'/>
+                <span>
+                  {format(date[0].endDate,"MM/dd/yyyy")}
+                </span>
+                <FontAwesomeIcon icon={faAngleDown} className='htAngle'/>
+              </div>
+              {((openStartDate && !openEndDate) || (!openStartDate && openEndDate))  && 
+                <DateRange
+                  onChange={item => setDate([item.selection])}
+                  minDate= {new Date()}
+                  moveRangeOnFirstSelection={false}
+                  className='calendarStart'
+                  ranges={date}
+              />}
+            </div>  
